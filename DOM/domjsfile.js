ed1406059771974f30e599a0d0cc94f89a168a4e
Carlos Batistela <cbhenry96@gmail.com>
2023-05-12 15:27:26 -0300

    Add basic DOM methods

---
+++ DOM/domjsfile.js
@@ -0,0 +1,39 @@
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text-content!';
+
+container.appendChild(content);
+
+const paragraph = document.createElement('p');
+paragraph.classList.add('p');
+paragraph.style.color = 'red';
+paragraph.textContent = "Hey I'm red!";
+
+container.appendChild(paragraph);
+
+const heading3 = document.createElement('h3');
+heading3.classList.add('h3');
+heading3.style.color = 'blue';
+heading3.textContent = "I'm a blue h3";
+
+container.appendChild(heading3);
+
+const newContent = document.createElement('div');
+
+const heading1 = document.createElement('h1');
+
+const paragraph2 = document.createElement('p');
+
+newContent.setAttribute('style', 'border: solid black; background: pink;');
+heading1.textContent = "I'm in a div";
+
+paragraph2.textContent = 'ME TOO!'
+
+container.appendChild(heading1);
+container.appendChild(paragraph2);
+
+newContent.append(heading1);
+newContent.append(paragraph2);
+container.appendChild(newContent);
